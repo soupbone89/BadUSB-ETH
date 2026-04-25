d64d85b89bf0a8feec3286fad6870b65db75b855
soupbone89 <217927327+soupbone89@users.noreply.github.com>
2026-04-25 16:16:25 +0300

    Create ms17-010.sh

---
+++ on_client/ms17-010.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+WAIT=2
+DPORT=445
+#https://github.com/worawit/MS17-010
+
+if nc -nw $WAIT $1 $DPORT < /dev/null 2> /dev/null; then
+	echo '[*] checking MS17-010'
+	nmap -Pn -n -p 445 --script smb-vuln-ms17-010 $1 > /tmp/ms17-010.log 2> /dev/null
+	if grep 'State: VULNERABLE' /tmp/ms17-010.log --color=auto;
+	then
+		led red on 2> /dev/null
+	fi
+fi
